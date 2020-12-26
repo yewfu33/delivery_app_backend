75a4f3230f05bd21e78997d0319e6e0ab76794d8
yewfu33 <53574514+yewfu33@users.noreply.github.com>
2020-12-27 00:14:34 +0800

    work on signalR hub

--- Delivery_app/Hubs/TrackingHub.cs
+++ Delivery_app/Hubs/TrackingHub.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,20 +9,44 @@ namespace Delivery_app.Hubs
 {
     public class TrackingHub : Hub
     {
-        public Task LatLngToServer(double latitude, double longitude)
+        // id storage
+        private static Hashtable clientIdCollection = new Hashtable();
+
+        // function interpolate courier location to client
+        public async Task LatLngToServer(double latitude, double longitude, int clientId)
         {
-            return Clients.All.SendAsync("CouriersLocation", latitude, longitude);
+            if(clientIdCollection.ContainsKey(clientId))
+            {
+                await Clients.Client(clientIdCollection[clientId].ToString())
+                    .SendAsync("CourierLocation", latitude, longitude);
+            }
         }
 
+        // override the onConnected callback
         public override async Task OnConnectedAsync()
         {
-            await Clients.Caller.SendAsync("OnHubConnected", "Hub connected");
             await base.OnConnectedAsync();
+            await Clients.Caller.SendAsync("OnHubConnected", "Hub connected");
         }
+
+        // register the context id and client id into the hashtable storage
+        public void registerConnectionId(int uid)
+        {
+            if (clientIdCollection.ContainsKey(uid))
+                clientIdCollection[uid] = Context.ConnectionId;
+            else
+                clientIdCollection.Add(uid, Context.ConnectionId);
+        }
+
+        // return the connection id
+        public string GetConnectionId() => Context.ConnectionId;
 
         public override async Task OnDisconnectedAsync(Exception ex)
         {
             await base.OnDisconnectedAsync(ex);
+            // fix later on
+            if (clientIdCollection.ContainsKey(Context.ConnectionId))
+                clientIdCollection.Remove(Context.ConnectionId);
         }
     }
 }
