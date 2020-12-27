d710bc32f92ae15f8c25e0a5cdc6d3fe55d7a6ab
yewfu33 <53574514+yewfu33@users.noreply.github.com>
2020-12-27 22:37:23 +0800

    add new view

--- Delivery_app.web/Controllers/MainController.cs
+++ Delivery_app.web/Controllers/MainController.cs
@@ -9,6 +9,9 @@ using Delivery_app.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using NToastNotify;
+using Delivery_app.Entities;
+using Delivery_app.web.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Delivery_app.web.Controllers
 {
@@ -17,21 +20,38 @@ namespace Delivery_app.web.Controllers
     {
         private readonly ILogger<MainController> _logger;
         private readonly IToastNotification _toastNotification;
+        private readonly AppDbContext _context;
 
-        public MainController(ILogger<MainController> logger, IToastNotification toastNotification)
+        public MainController(ILogger<MainController> logger, 
+            IToastNotification toastNotification,
+            AppDbContext context)
         {
             _logger = logger;
             _toastNotification = toastNotification;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(DashboardViewModel model)
         {
-            return View();
-        }
+            try
+            {
+                IQueryable<Orders> orders = _context.orders;
+                model.totalOrdersCount = await orders.CountAsync();
 
-        public IActionResult Privacy()
-        {
-            return View();
+                IQueryable<Users> users = _context.users;
+                model.totalUsersCount = await users.CountAsync();
+
+                IQueryable<Couriers> couriers = _context.couriers;
+                model.totalCouriersCount = await couriers.CountAsync();
+
+                return View(model);
+            }
+            catch (Exception)
+            {
+                _toastNotification.AddErrorToastMessage("Failed to load the display data.");
+
+                return View(new DashboardViewModel());
+            }
         }
     }
 }
---
+++ Delivery_app.web/Models/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Delivery_app.web.Models
+{
+    public class DashboardViewModel
+    {
+        public int totalOrdersCount { get; set; }
+        public int totalCouriersCount { get; set; }
+        public int totalUsersCount { get; set; }
+        public int totalIncome { get; set; }
+    }
+}
